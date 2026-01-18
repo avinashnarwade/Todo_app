3de12360fdddaca0f0fec95a7637d53b02afa47f
avinashnarwade <avinashnarwade872@gmail.com>
2026-01-18 14:17:26 +0530

    Add these files

---
+++ todo/apps.py
@@ -0,0 +1,12 @@
+# from django.apps import AppConfig
+
+
+# class TodoConfig(AppConfig):
+#     name = 'todo'
+
+from django.apps import AppConfig
+
+
+class TodoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'todo'
